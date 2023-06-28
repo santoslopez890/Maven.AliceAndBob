0a50a37183c767fe2098a009e4deed0ad4e0d242
Santos <santos@Zipcoders-MacBook-Pro-9.local>
2023-06-28 18:56:11 -0400

    nfijfnsdi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+Scanner in=new Scanner(System.in);
+String name="";
+        System.out.print("Enter your name: ");
+        name = in.nextLine();
+        System.out.print("Welcome "+name);
 
     }
 }
